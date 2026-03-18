f2615a3d40ba8132e05843e4617c970de5d9df2b
NICOLAS BERNARDINO LEMOS <nicolas.lemos3@senaisp.edu.br>
2026-03-18 15:05:40 -0300

    salve

---
+++ src/Ex01.java
@@ -0,0 +1,67 @@
+void main() {
+
+    Scanner b22 = new Scanner(System.in);
+
+
+
+
+    IO.println("Qual seu Nome?");
+
+    String nome = b22.nextLine();
+
+
+
+    IO.println("Qual seu Curso?");
+
+  String curso = b22.nextLine();
+
+
+   IO.println("Qual sua idade?");
+
+    int idade = b22.nextInt();
+
+
+    IO.println("Cadastro completo");
+
+    IO.println("------Suas informações:");
+    IO.println("\n");
+    IO.println( "seu nome: "+ nome + "\n" + "seu curso: " + curso + "\n" + "sua idade: " + idade + "\n" );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
